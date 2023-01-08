d33629ef027b4a3f9ee1bd9f4c25dd3adc2c77b7
shahin <shahin.shahi1986@gmail.com>
2023-01-09 00:14:12 +0330

    upload project

---
+++ Main/assets/js/questions.js
@@ -0,0 +1,30 @@
+// list of all questions, choices, and answers
+var questions = [
+  {
+    title: 'Which choice is not a front-end programming language?',
+    choices: ['React js', 'Python', 'Angular', 'Vue js'],
+    answer: 'Python',
+  },
+  {
+    title: 'Which language is suitable for mobile application?',
+    choices: ['Vue js', 'Javascript', 'Java', 'golang'],
+    answer: 'Java',
+  },
+  {
+    title: 'In what year was the JavaScript language unveiled?',
+    choices: [
+      '1990','1993','1998','2001',
+    ],
+    answer: '1990',
+  },
+  {
+    title: 'Who is the creator of JavaScript language?',
+    choices: ['Brendan Eich', 'James Arthur Gosling', 'Guido van Rossum', 'Rasmus Lerdorf'],
+    answer: 'Brendan Eich',
+  },
+  {
+    title:'Which is a php framework?',
+    choices: ['fluter', 'laravel', 'django', 'solidity'],
+    answer: 'laravel',
+  },
+];
